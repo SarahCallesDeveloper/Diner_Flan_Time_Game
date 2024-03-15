03d4a78004b7fc16b0db510fdfe7527dfef214bf
SarahCallesDeveloper <sarahcallesdeveloper@gmail.com>
2024-03-15 20:37:59 -0300

    Added Character, character movement, pixelart and mask , Basiccollision(print)

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 SCREEN_WIDTH = 720
 SCREEN_HEIGHT = 480
@@ -9,11 +10,27 @@ pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Diner Flan Time!")
-# Load images for background
+
 floor_image = pygame.image.load("./Images/Background/Floor.png").convert_alpha()
 left_wall_image = pygame.image.load("./Images/Background/LeftWall.png").convert_alpha()
 middle_wall_image = pygame.image.load("./Images/Background/MiddleWall.png").convert_alpha()
 right_wall_image = pygame.image.load("./Images/Background/RightWall.png").convert_alpha()
+
+left_wall_rect = pygame.Rect(0, 0, WALL_WIDTH, SCREEN_HEIGHT)
+middle_wall_rect = pygame.Rect(WALL_WIDTH, 0, WALL_WIDTH, SCREEN_HEIGHT)
+right_wall_rect = pygame.Rect(SCREEN_WIDTH - WALL_WIDTH, 0, WALL_WIDTH, SCREEN_HEIGHT)
+
+left_wall_mask = pygame.mask.from_surface(left_wall_image)
+
+
+character_image = pygame.image.load(os.path.join("./Images\Characters\Waitress\Waitress.png")).convert_alpha()
+character_rect = character_image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+
+character_mask= pygame.mask.from_surface(character_image)
+
+
+
+clock = pygame.time.Clock()
 
 running = True
 while running:
@@ -21,23 +38,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        character_rect.x -= 5
+    if keys[pygame.K_RIGHT]:
+        character_rect.x += 5
+    if keys[pygame.K_UP]:
+        character_rect.y -= 5
+    if keys[pygame.K_DOWN]:
+        character_rect.y += 5
+
     screen.fill((0, 0, 0))
-
-    # Blit floor image
     screen.blit(floor_image, (0, SCREEN_HEIGHT // 4.6))
-    
-    # Blit left wall image
     screen.blit(left_wall_image, (0, 0))
-
-    # Blit middle wall image
-
     screen.blit(middle_wall_image, (WALL_WIDTH, 0))
-
-    # Blit right wall image
     screen.blit(right_wall_image, (SCREEN_WIDTH - WALL_WIDTH, 0))
+    screen.blit(character_image, character_rect)
 
+   # if character_mask.overlap(left_wall_image(0) - character_rect.x,left_wall_image(0) - character_rect.y )
+    if character_mask.overlap(left_wall_mask, ( left_wall_rect.x - character_rect.x, left_wall_rect.y - character_rect.y )):
+        print("COLISSIOOON")
     pygame.display.flip()
-
-    pygame.time.Clock().tick(60)
+    clock.tick(60)
 
 pygame.quit()
